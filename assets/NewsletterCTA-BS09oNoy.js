0e7d01dfbf2936d506676a0d5822e234dfdb5107
Radwa85 <radwa.mohamed5033@gmail.com>
2026-07-04 14:55:27 +0300

    Updates

---
+++ assets/NewsletterCTA-BS09oNoy.js
@@ -0,0 +1,34 @@
+import{n as e,r as t}from"./index-mYAp36Nd.js";t();var n=`/plantified/assets/news-planet-CTF12ti1.webp`,r=e(),i=()=>(0,r.jsx)(`section`,{className:`w-full flex justify-center px-4 lg:px-[64px] mb-12 lg:mb-[74px] max-w-7xl mx-auto`,children:(0,r.jsxs)(`div`,{className:`\r
+          relative \r
+          w-full \r
+          h-[232px]\r
+          bg-[#363D37] \r
+          rounded-[24px] \r
+          flex \r
+          flex-col \r
+          items-center \r
+          justify-center \r
+          py-[32px] \r
+          px-[24px] \r
+          overflow-hidden\r
+        `,children:[(0,r.jsx)(`div`,{className:`absolute right-[5%] md:right-[19%] top-0 bottom-0 w-[150px] md:w-[240px]  pointer-events-none bg-no-repeat bg-center`,style:{backgroundImage:`url(${n})`,backgroundSize:`contain`}}),(0,r.jsxs)(`div`,{className:`relative z-10 flex flex-col items-center gap-[24px] w-full max-w-[850px] text-center`,children:[(0,r.jsxs)(`div`,{className:`flex flex-col gap-[12px]`,children:[(0,r.jsx)(`h2`,{className:`text-white/[0.88] font-medium text-[20px] `,children:`Find the plant that grows with you.`}),(0,r.jsx)(`p`,{className:`text-white/[0.66] text-[18px] font-regular  `,children:`Join 50,000+ urban dwellers who have rediscovered the joy of quiet growth. Delivered with care, straight to your door.`})]}),(0,r.jsxs)(`form`,{onSubmit:e=>{e.preventDefault()},className:`flex items-center gap-[8px] w-full max-w-[425px]`,children:[(0,r.jsx)(`input`,{type:`email`,placeholder:`Enter your email here`,required:!0,className:`\r
+                flex-1 \r
+                h-[48px] \r
+                bg-white/[0.12] \r
+                border border-white/[0.12] \r
+                rounded-[24px] \r
+                px-[24px] \r
+                text-white/[0.88] \r
+                placeholder:text-white/[0.44] \r
+                outline-none \r
+                transition-all\r
+              `}),(0,r.jsx)(`button`,{type:`submit`,className:`\r
+                h-[48px] \r
+                px-[32px] \r
+                bg-[#D3D4D3] \r
+                text-[#346E3C] \r
+                font-medium \r
+                text-[16px] \r
+                rounded-[24px] \r
+                transition-colors \r
+              `,children:`Send`})]})]})]})});export{i as NewsletterCTA};
